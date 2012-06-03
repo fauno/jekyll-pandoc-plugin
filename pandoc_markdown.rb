bddf1c0414006d19dc722d85f674f1ea0589bb0c
David Sanson <dsanson@gmail.com>
2012-06-03 19:45:26 -0400

    override markdown config if pandoc options are set

--- pandoc_markdown.rb
+++ pandoc_markdown.rb
@@ -3,7 +3,7 @@ require 'pandoc-ruby'
 class Jekyll::MarkdownConverter
 
   def convert(content)
-    return super unless @config['markdown'] == 'pandoc'
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
     @pandoc_extensions = @config['pandoc']['extensions'].map { |e| e.to_sym }
     PandocRuby.new(content, *@pandoc_extensions).to_html
   end
