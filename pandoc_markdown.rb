8dd292f483cb81e008d769b4588f4cfb118b1d11
David Sanson <dsanson@gmail.com>
2013-01-25 11:19:19 -0600

    convert to html5 instead of html

--- pandoc_markdown.rb
+++ pandoc_markdown.rb
@@ -5,6 +5,6 @@ class Jekyll::MarkdownConverter
   def convert(content)
     return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
     @pandoc_extensions = @config['pandoc']['extensions']
-    PandocRuby.new(content, *@pandoc_extensions).to_html
+    PandocRuby.new(content, *@pandoc_extensions).to_html5
   end
 end
